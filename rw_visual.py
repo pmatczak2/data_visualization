395be794c8b87076a9d07539424ca7ac5965ecd6
piotrmatczak <pmatczak2@gmail.com>
2021-10-23 13:05:50 -0700

    altering the size to fill the screen

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,7 @@ while True:
     rw.fill_walk()
 
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                edgecolors='none', s=1)
