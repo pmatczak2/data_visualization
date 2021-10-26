d1486e69b0d2b8139d36cb0902dd535f8daef2ed
piotrmatczak <pmatczak2@gmail.com>
2021-10-26 16:08:41 -0700

    making histogram

--- die_visual.py
+++ die_visual.py
@@ -9,6 +9,7 @@ results = []
 for roll_num in range(1000):
     result = die.roll()
     results .append(result)
+
 # analyze the result
 frequencies = []
 for value in range(1, die.num_sides+1):
@@ -16,13 +17,13 @@ for value in range(1, die.num_sides+1):
     frequencies.append(frequency)
 
 # visualize the result
-x_values = list(range(1, die.num_sides+1))
-data = [Bar(x=x_values, y=frequencies)]
+x_values = list(range(1, die.num_sides+1)) # plotly doesn't accept range() function directly, convert to list() function
+data = [Bar(x=x_values, y=frequencies)]  # Bar() represents a data set that will be formatted as whole
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result'} # Layout() returns object specified, configuration of the graph as a whole
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling on D6 1000 times',
-                   xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
-
+                   xaxis=x_axis_config, yaxis=y_axis_config) # Set the title of graph, configuration dictionary
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html') # offline.plot need dictionary containing the data
+            #and also accepts a dictionary containing the data and layout objects also the name where file will be saved
 print(frequencies)
