379c625b5e20cc2fbca0af90c0ba5d7b57aa2097
piotrmatczak <pmatczak2@gmail.com>
2021-10-26 16:14:08 -0700

    Rolling two dice

--- die_visual.py
+++ die_visual.py
@@ -3,27 +3,30 @@ from plotly import offline
 
 from die import Die
 
-die = Die()
+# create two D6 dice.
+die_1 = Die()
+die_2 = Die()
 
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results .append(result)
 
 # analyze the result
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # visualize the result
-x_values = list(range(1, die.num_sides+1)) # plotly doesn't accept range() function directly, convert to list() function
+x_values = list(range(2, max_result+1)) # plotly doesn't accept range() function directly, convert to list() function
 data = [Bar(x=x_values, y=frequencies)]  # Bar() represents a data set that will be formatted as whole
 
-x_axis_config = {'title': 'Result'} # Layout() returns object specified, configuration of the graph as a whole
+x_axis_config = {'title': 'Result', 'dtick': 1} # Layout() returns object specified, configuration of the graph as a whole
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling on D6 1000 times',
+my_layout = Layout(title='Results of rolling two D6 1000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config) # Set the title of graph, configuration dictionary
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html') # offline.plot need dictionary containing the data
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html') # offline.plot need dictionary containing the data
             #and also accepts a dictionary containing the data and layout objects also the name where file will be saved
 print(frequencies)
