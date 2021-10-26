ccdb25d05b8bdbd0a6712b9ac8c4107c49a8279d
piotrmatczak <pmatczak2@gmail.com>
2021-10-26 16:40:34 -0700

    Rolling dice of different size

--- die_visual.py
+++ die_visual.py
@@ -5,10 +5,10 @@ from die import Die
 
 # create two D6 dice.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results .append(result)
 
@@ -25,8 +25,8 @@ data = [Bar(x=x_values, y=frequencies)]  # Bar() represents a data set that will
 
 x_axis_config = {'title': 'Result', 'dtick': 1} # Layout() returns object specified, configuration of the graph as a whole
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times',
+my_layout = Layout(title='Results of rolling two D6 50,000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config) # Set the title of graph, configuration dictionary
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html') # offline.plot need dictionary containing the data
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html') # offline.plot need dictionary containing the data
             #and also accepts a dictionary containing the data and layout objects also the name where file will be saved
 print(frequencies)
