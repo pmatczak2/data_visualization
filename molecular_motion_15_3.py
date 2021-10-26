05caeba90123ad328d2e1a607bffd718efd43d02
piotrmatczak <pmatczak2@gmail.com>
2021-10-25 18:42:48 -0700

    15-5 refactoring

--- molecular_motion_15_3.py
+++ molecular_motion_15_3.py
@@ -1,21 +1,20 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-while True:
-    rw = RandomWalk()
-    rw.fill_walk()
+#while True:
+rw = RandomWalk()
+rw.fill_walk()
 
-    plt.style.use('seaborn')
-    fig, ax = plt.subplots()
-    point_numbers = range(rw.num_points)
-    ax.plot(rw.x_values, rw.y_values, linewidth=3)
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+point_numbers = range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, s=1)
 
-    #ax.get_xaxis().set_visible(False)
-    #ax.get_yaxis().set_visible(False)
+#ax.get_xaxis().set_visible(False)
+#ax.get_yaxis().set_visible(False)
 
-    plt.show()
+plt.show()
 
-    keep_running = input("Make another walk? (y/n): ")
-    if keep_running == 'n':
-        break
-
+    #keep_running = input("Make another walk? (y/n): ")
+    #if keep_running == 'n':
+    #   break
--- random_walk.py
+++ random_walk.py
@@ -1,22 +1,22 @@
 from random import choice
 
 class RandomWalk:
-    def __init__(self, num_points=5000):
+    def __init__(self, num_points=50_000):
         self.num_points = num_points
         self.x_values = [0]
         self.y_values = [0]
-    
-    
+
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([1, 2, 3, 4])
+        step = direction * distance
+        return step
+
     def fill_walk(self):
         while len(self.x_values) < self.num_points: #simulate 4 different direction.
-    
-            x_direction = choice([1, -1])#choose a value for x_direction, returns 1 for right or -1 for let
-            x_distance = choice([0, 1, 2, 3, 4])#here tells Python how far to move in that direction
-            x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             if x_step == 0 and y_step == 0:
                 continue
