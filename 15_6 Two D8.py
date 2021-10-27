ad258f5900bc579afe02b039a80138fc4305d96f
piotrmatczak <pmatczak2@gmail.com>
2021-10-27 04:29:57 -0700

    Try it yourself 15-6 Two D8

---
+++ 15_6 Two D8.py
@@ -0,0 +1,12 @@
+from random import randint
+
+class TwoD8:
+
+    def __init__(self, num_sides=8):
+        self.num_sides = num_sides
+
+    def roll(self):
+        return randint(1, self.num_sides)
+
+d8 = TwoD8()
+print(d8.roll())
