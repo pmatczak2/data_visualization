330cd61e0ffe8644b8cfa713ca75c87a7ff3ce8b
piotrmatczak <pmatczak2@gmail.com>
2021-10-27 16:34:21 -0700

    15-8 multiplication

--- two_die_vis.py
+++ two_die_vis.py
@@ -5,24 +5,24 @@ from two_D8 import TwoD8
 
 die_1 = TwoD8()
 die_2 = TwoD8()
-die_3 = TwoD8()
+
 
 
 # Make some rolls, and store results in a list
 results = []
 for roll_num in range(1_000_000):
-    result = die_1.roll() + die_2.roll() + die_3.roll()
+    result = die_1.roll() * die_2.roll()
     results.append(result)
 
-# Analize the results.
+# Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
-for value in range(3, max_result+1):
+max_result = die_1.num_sides * die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(3, max_result+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Results', 'dtick': 1}
