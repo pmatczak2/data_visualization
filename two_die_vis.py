55dd4d3c49552c1dd627b18308a66c77cf74e63e
piotrmatczak <pmatczak2@gmail.com>
2021-10-27 15:42:57 -0700

    Try it yourself 15-6 Two D8

--- two_die_vis.py
+++ two_die_vis.py
@@ -23,3 +23,8 @@ for value in range(2, max_result+1):
 x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
+x_axis_config = {'title': 'Results', 'dtick': 1}
+y_axis_config = {'Title': 'Frequency of Result'}
+my_layout = Layout(title='Result of rolling two D8 die 1000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d8_html')
