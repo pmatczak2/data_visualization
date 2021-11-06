52631f45189b6684071348c58816aeac76871f78
piotrmatczak <pmatczak2@gmail.com>
2021-11-06 15:55:16 -0700

    plotting data in a temperature chart

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,15 +1,27 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     print(header_row)
 
-
     highs = []
     for row in reader:
         high = int(row[5])
         highs.append(high)
 
-print(highs)
+    # Plot the high temperatures.
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    ax.plot(highs, c='red')
+
+    # Format plot.
+    ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+    ax.set_xlabel('', fontsize=16)
+    ax.set_ylabel("Temperature (F)", fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
