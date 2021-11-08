ae662ced602475cc56ec85b4ffc6e8b782920e16
piotrmatczak <pmatczak2@gmail.com>
2021-11-07 17:06:13 -0800

    Shading an area in the chart with fill_between()

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -22,8 +22,9 @@ with open(filename) as f:
     # Plot the high and low temperatures.
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    ax.plot(dates, highs, c='red')
-    ax.plot(dates, lows, c='blue')
+    ax.plot(dates, highs, c='red', alpha=0.5)
+    ax.plot(dates, lows, c='blue', alpha=0.5)
+    ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1) # Shading an area in the chart with fill_between()
 
     # Format plot.
     ax.set_title("Daily high, and low temperatures - 2018", fontsize=24)
