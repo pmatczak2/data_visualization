a55f114f29481818769c167632a01c96145dc042
piotrmatczak <pmatczak2@gmail.com>
2021-11-08 04:45:01 -0800

    format plot

--- sitka_rainfall.py
+++ sitka_rainfall.py
@@ -8,5 +8,20 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header )
+    # Get date and rainfall from this file.
+    dates, rainfalls = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        rainfall = [4]
+        dates.append(current_date)
+        rainfalls.append(rainfall)
+
+# Plot the high temperature
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+sx.plot(dates, rainfalls, c='blue')
+
+# Format plot
+ax.set_title("Daily rainfall for 2018", fontsize=24)
+ax.set_xlabel('', fontsizw=16)
+fig.autofmt_xdate()
