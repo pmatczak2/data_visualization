5afec718f9baa63823e07de2893ea0b49c34e271
piotrmatczak <pmatczak2@gmail.com>
2021-11-08 04:32:13 -0800

    sitka rainfall

---
+++ sitka_rainfall.py
@@ -0,0 +1,12 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header )
