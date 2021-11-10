3ca1afe2f47bf1094fa46703e0afd583f654e50d
piotrmatczak <pmatczak2@gmail.com>
2021-11-09 17:30:02 -0800

    Plot Sitka weather data.

--- sitka_death_valley_comparison.py
+++ sitka_death_valley_comparison.py
@@ -25,4 +25,9 @@ file_name = 'data/sitka_weather_2018_simple.csv'
 dates, highs, lows = [], [], []
 get_weather_data(file_name, dates, highs, lows, date_index=2, high_index=5, low_index=6)
 
-
+# Plot Sitka weather data.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.6)
+ax.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
