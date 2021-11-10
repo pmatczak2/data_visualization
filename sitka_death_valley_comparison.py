8ec060e86abf57e460cf358d479a6c2aeb2f660e
piotrmatczak <pmatczak2@gmail.com>
2021-11-09 17:10:09 -0800

    Get dates, and high and low temperatures from this file.

--- sitka_death_valley_comparison.py
+++ sitka_death_valley_comparison.py
@@ -8,4 +8,12 @@ def get_weather_data(filename, dates, highs, lows, date_index, high_index, low_i
         header_row = next(reader)
 
         # Get dates, and high and low temperatures from this file.
+        dates, high, lows = [], [], []
+        for row in reader:
+            current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+            high = int(row[5])
+            low = int(row[6])
+            dates.append(current_date)
+            dates.append(high)
+            dates.append(low)
 
