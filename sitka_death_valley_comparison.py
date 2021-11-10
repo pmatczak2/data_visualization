9f839551a98cad57574b38ea2c19fef718c03ea8
piotrmatczak <pmatczak2@gmail.com>
2021-11-09 16:53:31 -0800

    creating a function get_weather_data()

--- sitka_death_valley_comparison.py
+++ sitka_death_valley_comparison.py
@@ -2,4 +2,10 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-def get_weather_data()
+def get_weather_data(filename, dates, highs, lows, date_index, high_index, low_index):
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        # Get dates, and high and low temperatures from this file.
+
