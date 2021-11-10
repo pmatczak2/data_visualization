41640c125028efc970be1b2678d0b17bd201a752
piotrmatczak <pmatczak2@gmail.com>
2021-11-09 18:22:27 -0800

    san francisco air temp high and low for 2021

---
+++ san_fransisco_temperature.py
@@ -0,0 +1,49 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+def get_weather_data(filename, dates, highs, lows, date_index, high_index, low_index):
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        # Get dates, and high and low temperatures from this file.
+        for row in reader:
+            current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+            try:
+                high = int(row[high_index])
+                low = int(row[low_index])
+            except ValueError:
+                print(f"Missing data for {current_date}")
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
+
+    # Get weather data for San Fran
+file_name = 'data/san_fran_temp.csv'
+dates, highs, lows = [], [], []
+get_weather_data(file_name, dates, highs, lows, date_index=2, high_index=3, low_index=4)
+
+# Plot Sitka weather data.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.6)
+ax.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
+
+# Format plot.
+title = "Daily Highs and Lows Temperature"
+title += "\nSan Fran, CA"
+
+ax.set_title(title, fontsize=20)
+ax.set_title("Daily high, and low temperatures - 2018", fontsize=24)
+plt.title(title, fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 130)
+
+plt.show()
