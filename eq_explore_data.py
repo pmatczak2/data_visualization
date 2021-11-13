8b97ecb17a3a7aa9434997af600f2f39a135f400
piotrmatczak <pmatczak2@gmail.com>
2021-11-13 07:30:21 -0800

    Extracting Magnitudes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -14,9 +14,16 @@ all_eq_data = all_eq_data['features']
 print(len(all_eq_data))
 
 # Extracting Magnitudes
-mags = []
-for all_eq_data in all_eq_data:
-    mag = all_eq_data['properties']['mag']
+# Extracting Location Data
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_data:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
