5fc32bec868b180f3079dc97437ba4c88c0b1165
piotrmatczak <pmatczak2@gmail.com>
2021-11-19 04:57:05 -0800

    world fires

--- world_fires.py
+++ world_fires.py
@@ -4,7 +4,14 @@ from datetime import datetime
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+num_rows = 10_000
+
 filename= 'data/world_fires_1_day.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+
+    #  Get brightness, lats, lons, and dates
+    dates, brightnesses = [], []
+    lats, lons = [], []
+
