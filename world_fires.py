8b3c0b04dd41159aaac3a90807f0cc5055f4c495
piotrmatczak <pmatczak2@gmail.com>
2021-11-20 15:51:55 -0800

    Get dates, brightness, longs, lats.

--- world_fires.py
+++ world_fires.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-num_rows = 10_000
+num_rows = 100
 
-filename= 'data/world_fires_1_day.csv'
+filename = 'data/world_fires_1_day.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -14,11 +14,18 @@ with open(filename) as f:
     print(header_row)
     #  Get dates, brightness, longs, lats.
     dates, brightnesses = [], []
-    lats, longs = [], []
+    lats, lons = [], []
+    row_num = 0
     for row in reader:
-        current_date = datetime.strptime(row[5], '%Y-%m-%d')
+        date = datetime.strptime(row[5], '%Y-%m-%d')
         brightness = float(row[2])
-        dates.append(current_date)
-        brightnesses.append(brightness)
 
+        dates.append(date)
+        brightnesses.append(brightness)
+        lats.append(row[0])
+        lons.append(row[1])
+
+        row_num += 1
+        if row_num == num_rows:
+            break
 
