c20f9c78308a368471ed5e65aed3b06ea9339e61
piotrmatczak <pmatczak2@gmail.com>
2021-11-22 04:50:59 -0800

    mapping the world fires

--- world_fires.py
+++ world_fires.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-num_rows = 100
+num_rows = 1_000
 
 filename = 'data/world_fires_1_day.csv'
 with open(filename) as f:
@@ -30,10 +30,21 @@ with open(filename) as f:
             break
 
 # Map the world fires
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+        'lon': lons,
+        'lat': lats,
+        'marker': {
+            'size': [brightness for brightness in brightnesses],
+            'color': brightnesses,
+            'colorscale': 'YlOrRd',
+            'reversescale': True,
+            'colorbar': {'title': 'Brightness'}
+        },
+}]
 my_layout = Layout(title='World Fires')
 
-fig = {'data':data, 'layout':my_layout}
+fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='world_fires.html')
 
 
